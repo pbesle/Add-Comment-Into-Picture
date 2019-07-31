88caa3882ac10529cd43776ce6b7a470ee36e845
Besle Pascal <pascal.besle@dxc.com>
2019-07-31 09:27:08 +0200

    Initialization

---
+++ CreateFrame.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python3
+
+import sys
+import os
+import csv
+from array import *
+
+Frame=[]
+FileType=".suf"
+TabFileType=[]
+NbPictures=0
+noexist=False
+
+def AddFrame(Picture):
+	flag_find=0
+	i=0
+	print("Picture = " + Picture)
+	for i in range(int(NbPictures)):
+		if Picture.find(Frame[i][0]) == 0:
+			flag_find=1
+			break
+	if (i == int(NbPictures) - 1 and flag_find == 0) or noexist == True:
+		Frame.append([Picture,0])
+		print("*** Add : " + str(Frame[-1][0]))
+
+
+def printdir(directory):
+	for dossier, sous_dossiers, files in os.walk(directory):
+		for file in files:
+			Picture=os.path.join(dossier, file)
+			FileType=str.lower(Picture[-4:])
+			for Type in range(NbFileType):
+				if TabFileType[Type] == FileType:
+					AddFrame(Picture)
+					break
+
+
+if len(sys.argv) < 4:
+        print("Usage : CreateFrame <Frame> <Root> <type1> [type2 ... typen]\n")
+        sys.exit(0)
+
+NbFileType=len(sys.argv)-3
+for Type in range(NbFileType):
+	TabFileType.append(sys.argv[3+Type])
+
+os.chdir(sys.argv[1])
+
+try:
+	with open("frame.csv",'r') as fp_frame:
+		ligne=csv.reader(fp_frame,delimiter=';')
+		for Pictures in ligne:
+			Frame.append([Pictures[0],Pictures[1]])
+			chaine=Frame[-1][0]
+			print("**" + str(chaine))
+	NbPictures=str(len(Frame))
+
+except IOError:
+	noexist=True
+	print ("INEXISTANT")
+
+printdir(os.sys.argv[2])
+
+with open("frame.csv",'w') as fp_frame:
+	writer=csv.writer(fp_frame,delimiter=";",lineterminator='\n')
+	writer.writerows(Frame)
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # Add-Comment-Into-Picture
-Add a comment into picture for running on numeric cadre
+Add a comment into picture for running on numeric frame
+
+These programs are written in python
+
+They are running on windows 10 with CYGWIN
+
+They browse a tree containing pictures (.jpg, .tiff, .bmp)  and add the directory name and the comment exif if it is present into the picture.
+
+## Prerequisite
+- Windows 10
+- Cygwin 64
+- python 3.6 (Intalled with cygwin Setup)
+- ImageMagic (installed with cygwin Setup)
+- pip (python-pip-whell installed with cygwin Setup)
+- Exifreader (installed with PIP)
+
+## Programs :
+
+- CreateFrame.py
+
+  This program browse a Picture Directory to add new pictures into a file named Cadre.csv located in the dorectory Frame_Name
+  
+  Usage : CreateFrame.py Frame_Name Picture_Directory_Name Picture_type1 (Picture_type2 ... Picture_typen)
+  
+  example : python3 ./CreateFrame.py "Frame Josefina" "/cygdrive/z/Mes Photos/Mes Photos" .jpg .bmp .png .tif
+  
+- NewCadre.py 
+
+  This program ramdomly choose picture into the file Frame.csv and add the directory name and the exif comment at the bottom of the picture
+  
+  Usage : NewFrame.py Frame_Name Number_Of_Pictures
+  
+  Example : python3 ./NewFrame.py "Cadre Josefina" 100
+
+![Rob Demo](pictures/ULM.jpg)
+
